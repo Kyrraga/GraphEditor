d9d7a40288f4f1db766c5ddd99c230a142787b2e
Kinrany <kinrany@yandex.com>
2016-10-22 09:17:37 +0300

    Created a solution with .dll project

---
+++ GraphModel/GraphModel/GraphModel.cs
@@ -0,0 +1,22 @@
+﻿using QuickGraph;
+
+namespace GraphModel
+{
+	public class Vertex {
+		public string name;
+		public string color;
+	}
+
+ public class GraphModel {
+		public void Load(string path) {
+
+		}
+
+
+		private CompressedSparseRowGraph<Vertex> graph;
+
+		private GraphModel() {
+
+		}
+	}
+}
