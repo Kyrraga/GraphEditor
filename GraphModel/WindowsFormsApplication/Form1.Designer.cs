a6f0f066ea2804ef7baaf764d99f7d6ae63feebc
Kinrany <kinrany@yandex.com>
2016-11-08 17:38:32 +0300

    Создал проект GUI на WinForms

---
+++ GraphModel/WindowsFormsApplication/Form1.Designer.cs
@@ -0,0 +1,56 @@
+﻿namespace WindowsFormsApplication {
+	partial class Form1 {
+		/// <summary>
+		/// Обязательная переменная конструктора.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Освободить все используемые ресурсы.
+		/// </summary>
+		/// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+		protected override void Dispose(bool disposing) {
+			if (disposing && (components != null)) {
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Код, автоматически созданный конструктором форм Windows
+
+		/// <summary>
+		/// Требуемый метод для поддержки конструктора — не изменяйте 
+		/// содержимое этого метода с помощью редактора кода.
+		/// </summary>
+		private void InitializeComponent() {
+			this.loadGraphButton = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// loadGraphButton
+			// 
+			this.loadGraphButton.Location = new System.Drawing.Point(28, 26);
+			this.loadGraphButton.Name = "loadGraphButton";
+			this.loadGraphButton.Size = new System.Drawing.Size(90, 23);
+			this.loadGraphButton.TabIndex = 0;
+			this.loadGraphButton.Text = "Открыть граф";
+			this.loadGraphButton.UseVisualStyleBackColor = true;
+			this.loadGraphButton.Click += new System.EventHandler(this.loadGraphButton_Click);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(887, 474);
+			this.Controls.Add(this.loadGraphButton);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Button loadGraphButton;
+	}
+}
+
---
+++ GraphModel/WindowsFormsApplication/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using GraphModel;
+
+namespace WindowsFormsApplication {
+	public partial class Form1 : Form {
+		public Form1() {
+			InitializeComponent();
+		}
+
+		private GraphModel.GraphModel graphModel = GraphModel.GraphModel.Load();
+
+		private void loadGraphButton_Click(object sender, EventArgs e) {
+			OpenFileDialog dialog = new OpenFileDialog();
+			dialog.FileOk += LoadGraph;
+			dialog.ShowDialog();
+		}
+
+		private void LoadGraph(object sender, CancelEventArgs e) {
+			
+		}
+	}
+}
