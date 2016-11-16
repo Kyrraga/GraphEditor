3b6e6a3c4970f350c99551d2d325676fd72f1018
Kinrany <kinrany@yandex.com>
2016-11-16 11:00:59 +0300

    TODO

---
+++ GraphModel/GraphModel/GraphModel2.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GraphModelLibrary {
+	class GraphModelA {
+		//TODO: класс для хранения модели в соответствии с форматом A1
+		//      парсер в отдельном классе
+	}
+}
