ebe8675b7bdec9d08746bb722d41b56122a892de
Kinrany <kinrany@yandex.com>
2016-11-16 10:48:09 +0300

    Классы Graph, Node, Edge для хранения графов.

---
+++ GraphModel/GraphModel/Edge.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+
+namespace GraphModelLibrary {
+	interface IEdge {
+		NodeColor Color { get; set; }
+		INode NodeFrom { get; }
+		INode NodeTo { get; }
+		void Delete();
+	}
+
+	class Edge : IEdge {
+		public IGraph Graph {
+			get {
+				return _graph;
+			}
+		}
+		public INode NodeFrom {
+			get {
+				return _from;
+			}
+		}
+		public INode NodeTo {
+			get {
+				return _to;
+			}
+		}
+		public string Value { get; set; }
+		public NodeColor Color { get; set; }
+
+		public Edge(IGraph graph, INode from, INode to) {
+			this._graph = graph;
+			this._from = from;
+			this._to = to;
+		}
+
+		public static IEdge Create(IGraph graph, INode from, INode to) {
+			return graph.AddEdge(from, to);
+		}
+
+		public ReadOnlyCollection<INode> Nodes {
+			get {
+				return Array.AsReadOnly(new INode[] { NodeFrom, NodeTo });
+			}
+		}
+
+		public void Delete() {
+			
+		}
+
+		private readonly IGraph _graph;
+		private readonly INode _from;
+		private readonly INode _to;
+	}
+}
---
+++ GraphModel/GraphModel/Node.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+
+namespace GraphModelLibrary {
+	interface INode {
+		NodeColor Color { get; set; }
+		IEnumerable<IEdge> GetIncomingEdges();
+		IEnumerable<IEdge> GetOutgoingEdges();
+		IEnumerable<IEdge> GetEdges();
+		IEnumerable<INode> GetAdjacentNodes(INode node);
+		void Delete();
+	}
+
+	class Node : INode {
+		public IGraph Graph {
+			get {
+				return _graph;
+			} 
+		}
+		public NodeColor Color { get; set; }
+
+		public Node(IGraph graph) {
+			this._graph = graph;
+		}
+
+		public static INode Create(IGraph graph) {
+			return graph.AddNode();
+		}
+
+		public IEnumerable<IEdge> GetIncomingEdges() {
+			return Graph.GetIncomingEdges(this);
+		}
+		public IEnumerable<IEdge> GetOutgoingEdges() {
+			return Graph.GetOutgoingEdges(this);
+		}
+		public IEnumerable<IEdge> GetEdges() {
+			return Graph.GetEdges(this);
+		}
+		public IEnumerable<INode> GetAdjacentNodes(INode node) {
+			return Graph.GetAdjacentNodes(this);
+		}
+		public void Delete() {
+			Graph.Remove(this);
+		}
+
+		private readonly IGraph _graph;
+	}
+}
