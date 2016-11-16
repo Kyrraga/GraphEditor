153e885bb180e29c5a6acde2064c438885618d42
Kinrany <kinrany@yandex.com>
2016-11-16 10:48:34 +0300

    Вынес NodeColors в отдельный файл.

--- GraphModel/GraphModel/GraphModel.cs
+++ GraphModel/GraphModel/GraphModel.cs
@@ -7,25 +7,6 @@ using ExtensionMethods;
 
 namespace GraphModelLibrary
 {
-	public enum NodeColor {
-		Magenta = 0,
-		Blue = 1,
-		Red = 2,
-		Green = 3,
-		Black = 4, 
-		White = 5,
-		Purple = 6,
-		Orange = 7,
-		Yellow = 8,
-		Pink = 9,
-		Turquoise = 10,
-		Grey = 11,
-		Teal = 12,
-		Darkblue = 13,
-		Violet = 14,
-		Brown = 15
-	}
-
 	public partial class GraphModel {
 		/// <summary>
 		/// Загружает граф из файла.
