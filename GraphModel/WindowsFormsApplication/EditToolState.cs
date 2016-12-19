d0c74081cd24831d4d3815ee11e74a104a7b2f16
Kinrany <kinrany@yandex.com>
2016-12-19 15:52:04 +0300

    New classes for editing tool

---
+++ GraphModel/WindowsFormsApplication/EditToolState.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+
+namespace WindowsFormsApplication {
+	class EditToolState {
+		public virtual EditToolState LeftMouseButtonDown(Point location) { return this; }
+		public virtual EditToolState LeftMouseButtonUp(Point location) { return this; }
+		public virtual EditToolState RightMouseButtonDown(Point location) { return this; }
+		public virtual EditToolState RightMouseButtonUp(Point location) { return this; }
+	}
+
+	class DefaultEditToolState : EditToolState {
+
+	}
+}
