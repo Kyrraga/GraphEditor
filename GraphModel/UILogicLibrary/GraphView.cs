0246b32677f82afc954b1494c130adff190d6fc3
Kinrany <kinrany@yandex.com>
2016-12-21 20:36:00 +0300

    Удалил ненужный код в библиотеке UI, починил юнит-тесты

--- GraphModel/UILogicLibrary/GraphView.cs
+++ GraphModel/UILogicLibrary/GraphView.cs
@@ -6,14 +6,12 @@ using System.Text;
 using GraphModelLibrary;
 
 namespace UILogicLibrary {
-	public interface IGraphView : IGraph {
+	public interface IGraphView {
 		int NodeRadius { get; set; }
-		INodeView GetNodeFrom(IEdgeView edge);
-		INodeView GetNodeTo(IEdgeView edge);
 	}
 
 	public class GraphView : IGraphView {
-		public GraphView(IGraph graph) {
+		public GraphView(Graph graph) {
 			this._graph = graph;
 		}
 
@@ -27,57 +25,9 @@ namespace UILogicLibrary {
 			}
 		}
 
-		public ReadOnlyCollection<INode> Nodes {
-			get {
-				return _graph.Nodes
-					.Select((node) => (new NodeView(this, node)) as INode)
-					.ToList()
-					.AsReadOnly();
-			}
-		}
-		public IEnumerable<IEdge> Edges {
-			get {
-				return _graph.Edges
-					.Select((edge) => (new EdgeView(this, edge) as IEdge));
-			}
-		}
+		
 
-		public IEnumerable<IEdge> GetIncomingEdges(INode node) {
-			throw new NotImplementedException();
-		}
-		public IEnumerable<IEdge> GetOutgoingEdges(INode node) {
-			throw new NotImplementedException();
-		}
-		public IEnumerable<IEdge> GetEdges(INode node) {
-			throw new NotImplementedException();
-		}
-		public IEnumerable<INode> GetAdjacentNodes(INode node) {
-			throw new NotImplementedException();
-		}
-
-		public INode AddNode() {
-			throw new NotImplementedException();
-		}
-		public IEdge AddEdge(INode nodeFrom, INode nodeTo) {
-			throw new NotImplementedException();
-		}
-
-		public void Remove(INode node) {
-			throw new NotImplementedException();
-		}
-		public void Remove(IEdge edge) {
-			throw new NotImplementedException();
-		}
-
-		public INodeView GetNodeFrom(IEdgeView edge) {
-			throw new NotImplementedException();
-		}
-
-		public INodeView GetNodeTo(IEdgeView edge) {
-			throw new NotImplementedException();
-		}
-
-		private IGraph _graph;
-		private int _nodeRadius = 15;
+		Graph _graph;
+		int _nodeRadius = 15;
 	}
 }
--- GraphModel/UnitTestProject/GraphModelUnitTest.cs
+++ GraphModel/UnitTestProject/GraphModelUnitTest.cs
@@ -58,19 +58,19 @@ Morbi elementum lorem et libero bibendum, ac egestas urna accumsan.";
 
 		[TestMethod]
 		public void Loading1() {
-			string path = @"C:\Users\Ruslan\Desktop\MAI\Диплом\GraphEditor\Examples\Graph files\exampleA1-1.txt";
+			string path = @"..\..\..\..\Examples\Graph files\exampleA1-1.txt";
 			GraphModel model = GraphModel.Load(path);
 		}
 
 		[TestMethod]
 		public void Loading2() {
-			string path = @"C:\Users\Ruslan\Desktop\MAI\Диплом\GraphEditor\Examples\Graph files\exampleA1-2.txt";
+			string path = @"..\..\..\..\Examples\Graph files\exampleA1-2.txt";
 			GraphModel model = GraphModel.Load(path);
 		}
 
 		[TestMethod]
 		public void Loading3() {
-			string path = @"C:\Users\Ruslan\Desktop\MAI\Диплом\GraphEditor\Examples\Graph files\exampleA1-3.txt";
+			string path = @"..\..\..\..\Examples\Graph files\exampleA1-3.txt";
 			GraphModel model = GraphModel.Load(path);
 		}
 	}
