77a2b7120130b59ce959d68ac875be086a9cd32b
Kinrany <kinrany@yandex.com>
2016-12-25 21:42:16 +0300

    Добавил возможность перетаскивать вершины.

--- GraphModel/UILogicLibrary/DefaultState.cs
+++ GraphModel/UILogicLibrary/DefaultState.cs
@@ -13,6 +13,10 @@ namespace UILogicLibrary {
 			CurrentState = new SelectionState(EditTool, location);
 		}
 
+		public override void MouseLeftPressed(NodeModel node) {
+			CurrentState = new DragState(EditTool, node);
+		}
+
 		public override void MouseLeftClick(Point location) {
 			NodeModel node = new NodeModel(location);
 			EditTool.GraphView.Graph.Add(node);
@@ -20,6 +24,10 @@ namespace UILogicLibrary {
 
 		public override void MouseLeftClick(NodeModel node) {
 			EditTool.SetSelected(node);
+		}
+
+		public override void MouseRightClick(Point location) {
+			EditTool.ClearSelected();
 		}
 	}
 }
---
+++ GraphModel/UILogicLibrary/DragState.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using GraphModelLibrary;
+
+namespace UILogicLibrary {
+	class DragState : EditToolState{
+		public DragState(EditTool editTool, NodeModel node) : base(editTool) {
+			this._node = node;
+		}
+
+		public override void MouseMoved(Point location) {
+			_node.Location = location;
+		}
+
+		public override void MouseLeftDepressed(NodeModel node) {
+			Depressed();
+		}
+
+		public override void MouseLeftDepressed(Point location) {
+			Depressed();
+		}
+
+		NodeModel _node;
+
+		void Depressed() {
+			CurrentState = new DefaultState(EditTool);
+		}
+	}
+}
