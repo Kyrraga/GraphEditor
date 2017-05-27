3319f9ee5c1de90dbdb3a33dd52c7f0f05c18e90
Kyrraga <velin256@gmail.com>
2017-05-27 15:26:03 +0300

    First version of dllloader(no buttons included).

--- GraphModel/WindowsFormsApplication/Form1.Designer.cs
+++ GraphModel/WindowsFormsApplication/Form1.Designer.cs
@@ -23,86 +23,106 @@
 		/// содержимое этого метода с помощью редактора кода.
 		/// </summary>
 		private void InitializeComponent() {
-			this.loadGraphButton = new System.Windows.Forms.Button();
-			this.graphBox = new System.Windows.Forms.GroupBox();
-			this.loadExampleButton = new System.Windows.Forms.Button();
-			this.debugLabel = new System.Windows.Forms.Label();
-			this.saveButton = new System.Windows.Forms.Button();
-			this.saveButtonLabel = new System.Windows.Forms.Label();
-			this.SuspendLayout();
-			// 
-			// loadGraphButton
-			// 
-			this.loadGraphButton.Location = new System.Drawing.Point(28, 26);
-			this.loadGraphButton.Name = "loadGraphButton";
-			this.loadGraphButton.Size = new System.Drawing.Size(104, 23);
-			this.loadGraphButton.TabIndex = 0;
-			this.loadGraphButton.Text = "Открыть граф";
-			this.loadGraphButton.UseVisualStyleBackColor = true;
-			this.loadGraphButton.Click += new System.EventHandler(this.loadGraphButton_Click);
-			// 
-			// graphBox
-			// 
-			this.graphBox.Location = new System.Drawing.Point(50, 96);
-			this.graphBox.Name = "graphBox";
-			this.graphBox.Size = new System.Drawing.Size(804, 336);
-			this.graphBox.TabIndex = 2;
-			this.graphBox.TabStop = false;
-			// 
-			// loadExampleButton
-			// 
-			this.loadExampleButton.Location = new System.Drawing.Point(28, 55);
-			this.loadExampleButton.Name = "loadExampleButton";
-			this.loadExampleButton.Size = new System.Drawing.Size(104, 23);
-			this.loadExampleButton.TabIndex = 3;
-			this.loadExampleButton.Text = "Открыть пример";
-			this.loadExampleButton.UseVisualStyleBackColor = true;
-			this.loadExampleButton.Click += new System.EventHandler(this.loadExampleButton_Click);
-			// 
-			// debugLabel
-			// 
-			this.debugLabel.AutoSize = true;
-			this.debugLabel.Location = new System.Drawing.Point(591, 452);
-			this.debugLabel.Name = "debugLabel";
-			this.debugLabel.Size = new System.Drawing.Size(74, 13);
-			this.debugLabel.TabIndex = 4;
-			this.debugLabel.Text = "<debug label>";
-			// 
-			// saveButton
-			// 
-			this.saveButton.Location = new System.Drawing.Point(195, 26);
-			this.saveButton.Name = "saveButton";
-			this.saveButton.Size = new System.Drawing.Size(75, 23);
-			this.saveButton.TabIndex = 5;
-			this.saveButton.Text = "Сохранить";
-			this.saveButton.UseVisualStyleBackColor = true;
-			this.saveButton.Click += new System.EventHandler(this.saveButton_Click);
-			// 
-			// saveButtonLabel
-			// 
-			this.saveButtonLabel.AutoSize = true;
-			this.saveButtonLabel.Location = new System.Drawing.Point(277, 35);
-			this.saveButtonLabel.Name = "saveButtonLabel";
-			this.saveButtonLabel.Size = new System.Drawing.Size(0, 13);
-			this.saveButtonLabel.TabIndex = 6;
-			// 
-			// Form1
-			// 
-			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(887, 474);
-			this.Controls.Add(this.saveButtonLabel);
-			this.Controls.Add(this.saveButton);
-			this.Controls.Add(this.debugLabel);
-			this.Controls.Add(this.loadExampleButton);
-			this.Controls.Add(this.graphBox);
-			this.Controls.Add(this.loadGraphButton);
-			this.DoubleBuffered = true;
-			this.Name = "Form1";
-			this.Text = "Form1";
-			this.Load += new System.EventHandler(this.Form1_Load);
-			this.ResumeLayout(false);
-			this.PerformLayout();
+            this.loadGraphButton = new System.Windows.Forms.Button();
+            this.graphBox = new System.Windows.Forms.GroupBox();
+            this.loadExampleButton = new System.Windows.Forms.Button();
+            this.debugLabel = new System.Windows.Forms.Label();
+            this.saveButton = new System.Windows.Forms.Button();
+            this.saveButtonLabel = new System.Windows.Forms.Label();
+            this.CodeImport = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // loadGraphButton
+            // 
+            this.loadGraphButton.Location = new System.Drawing.Point(37, 32);
+            this.loadGraphButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.loadGraphButton.Name = "loadGraphButton";
+            this.loadGraphButton.Size = new System.Drawing.Size(139, 28);
+            this.loadGraphButton.TabIndex = 0;
+            this.loadGraphButton.Text = "Открыть граф";
+            this.loadGraphButton.UseVisualStyleBackColor = true;
+            this.loadGraphButton.Click += new System.EventHandler(this.loadGraphButton_Click);
+            // 
+            // graphBox
+            // 
+            this.graphBox.Location = new System.Drawing.Point(67, 118);
+            this.graphBox.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.graphBox.Name = "graphBox";
+            this.graphBox.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.graphBox.Size = new System.Drawing.Size(1072, 414);
+            this.graphBox.TabIndex = 2;
+            this.graphBox.TabStop = false;
+            // 
+            // loadExampleButton
+            // 
+            this.loadExampleButton.Location = new System.Drawing.Point(37, 68);
+            this.loadExampleButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.loadExampleButton.Name = "loadExampleButton";
+            this.loadExampleButton.Size = new System.Drawing.Size(139, 28);
+            this.loadExampleButton.TabIndex = 3;
+            this.loadExampleButton.Text = "Открыть пример";
+            this.loadExampleButton.UseVisualStyleBackColor = true;
+            this.loadExampleButton.Click += new System.EventHandler(this.loadExampleButton_Click);
+            // 
+            // debugLabel
+            // 
+            this.debugLabel.AutoSize = true;
+            this.debugLabel.Location = new System.Drawing.Point(788, 556);
+            this.debugLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.debugLabel.Name = "debugLabel";
+            this.debugLabel.Size = new System.Drawing.Size(98, 17);
+            this.debugLabel.TabIndex = 4;
+            this.debugLabel.Text = "<debug label>";
+            // 
+            // saveButton
+            // 
+            this.saveButton.Location = new System.Drawing.Point(260, 32);
+            this.saveButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.saveButton.Name = "saveButton";
+            this.saveButton.Size = new System.Drawing.Size(100, 28);
+            this.saveButton.TabIndex = 5;
+            this.saveButton.Text = "Сохранить";
+            this.saveButton.UseVisualStyleBackColor = true;
+            this.saveButton.Click += new System.EventHandler(this.saveButton_Click);
+            // 
+            // saveButtonLabel
+            // 
+            this.saveButtonLabel.AutoSize = true;
+            this.saveButtonLabel.Location = new System.Drawing.Point(369, 43);
+            this.saveButtonLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.saveButtonLabel.Name = "saveButtonLabel";
+            this.saveButtonLabel.Size = new System.Drawing.Size(0, 17);
+            this.saveButtonLabel.TabIndex = 6;
+            // 
+            // CodeImport
+            // 
+            this.CodeImport.Location = new System.Drawing.Point(403, 32);
+            this.CodeImport.Name = "CodeImport";
+            this.CodeImport.Size = new System.Drawing.Size(130, 28);
+            this.CodeImport.TabIndex = 7;
+            this.CodeImport.Text = "Импорт кода";
+            this.CodeImport.UseVisualStyleBackColor = true;
+            this.CodeImport.Click += new System.EventHandler(this.CodeImport_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1183, 583);
+            this.Controls.Add(this.CodeImport);
+            this.Controls.Add(this.saveButtonLabel);
+            this.Controls.Add(this.saveButton);
+            this.Controls.Add(this.debugLabel);
+            this.Controls.Add(this.loadExampleButton);
+            this.Controls.Add(this.graphBox);
+            this.Controls.Add(this.loadGraphButton);
+            this.DoubleBuffered = true;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
 
 		}
 
@@ -114,6 +134,7 @@
 		private System.Windows.Forms.Label debugLabel;
 		private System.Windows.Forms.Button saveButton;
 		private System.Windows.Forms.Label saveButtonLabel;
-	}
+        private System.Windows.Forms.Button CodeImport;
+    }
 }
 
--- GraphModel/WindowsFormsApplication/Form1.cs
+++ GraphModel/WindowsFormsApplication/Form1.cs
@@ -11,10 +11,11 @@ using System.Windows.Forms;
 using GraphModelLibrary;
 using ExtensionMethods;
 using UILogicLibrary;
+using System.Runtime.InteropServices;
 
 namespace WindowsFormsApplication {
 	public partial class Form1 : Form {
-		public Form1() {
+        public Form1() {
 			InitializeComponent();
 		}
 
@@ -141,5 +142,141 @@ namespace WindowsFormsApplication {
 		Point ConvertPoint(Point p, Control c1, Control c2) {
 			return c2.PointToClient(c1.PointToScreen(p));
 		}
-	}
+
+        /*[UnmanagedFunctionPointer(CallingConvention.Winapi)]
+        [return: MarshalAs(UnmanagedType.I4)]
+        private delegate int main_t(
+        [MarshalAs(UnmanagedType.I4)] int index,
+        string message);*/
+
+        [UnmanagedFunctionPointer(CallingConvention.Winapi)]
+        [return: MarshalAs(UnmanagedType.I4)]
+        private delegate int mult_t([MarshalAs(UnmanagedType.I4)] int num);
+
+        [UnmanagedFunctionPointer(CallingConvention.Winapi)]
+        [return: MarshalAs(UnmanagedType.I4)]
+        private delegate int main_t([MarshalAs(UnmanagedType.I4)] int num, string path);
+
+        [UnmanagedFunctionPointer(CallingConvention.StdCall)]
+        [return: MarshalAs(UnmanagedType.LPStr)]
+        private delegate string solve_t([MarshalAs(UnmanagedType.LPStr)] string path);
+
+        [UnmanagedFunctionPointer(CallingConvention.Winapi)]
+        [return: MarshalAs(UnmanagedType.I4)]
+        private delegate string disp_t([MarshalAs(UnmanagedType.LPStr)] string path);
+
+        [UnmanagedFunctionPointer(CallingConvention.Winapi)]
+        [return: MarshalAs(UnmanagedType.I4)]
+        private delegate void print_t([MarshalAs(UnmanagedType.LPStr)]string path);
+
+        [UnmanagedFunctionPointer(CallingConvention.StdCall)]
+        [return: MarshalAs(UnmanagedType.LPStr)]
+        private delegate string adv_print_t([MarshalAs(UnmanagedType.LPStr)]string path);
+
+        //[DllImport(@"C:\Users\Vladimir\Desktop\dlltest\CCode\tryhard\x64\Release\tryhard.dll")]
+        //public static extern int MultiplyByTen(int num);
+
+        private void CodeImport_Click(object sender, EventArgs e)
+        {
+            //private const string DllFilePath = @"c:\pathto\mydllfile.dll";
+            
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            DialogResult result = openFileDialog.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                string path = openFileDialog.FileName;
+                string path_to_graph = @"C:\Users\Vladimir\Desktop\Diploma\GraphEditor\GraphModel\WindowsFormsApplication\Examples\exampleA1-3.txt";
+
+                /*dynaloader loader = new dynaloader(path);
+                main_t main = loader.load_function<main_t>("main");
+                main(1, path_to_graph);*/
+
+                dynaloader loader = new dynaloader(path);
+                solve_t solve = loader.load_function<solve_t>("solve");
+                //MessageBox.Show(solve(path_to_graph));
+
+                GraphModel = GraphModel.Load(solve(path_to_graph));
+
+                /*dynaloader loader = new dynaloader(path);
+                adv_print_t solve = loader.load_function<adv_print_t>("adv_print");
+                MessageBox.Show(solve(path_to_graph)); */
+
+                //dynaloader loader = new dynaloader(path);
+                //adv_print_t printer = loader.load_function<adv_print_t>("adv_print");
+                //printer(path_to_graph);
+
+                /*dynaloader loader = new dynaloader(path);
+                mult_t main = loader.load_function<mult_t>("MultiplyByTen");
+                MessageBox.Show(main(10).ToString());
+
+                dynaloader loader2 = new dynaloader(path2);
+                mult_t main2 = loader2.load_function<mult_t>("MultiplyByTwelve");
+                mult_t main3 = loader2.load_function<mult_t>("MultiplyByHundred");
+                MessageBox.Show(main2(10).ToString());
+                MessageBox.Show(main3(65536).ToString());*/
+
+
+            }
+        }
+    }
+
+    class dynaloader
+    {
+        private IntPtr m_dll = IntPtr.Zero;
+
+        public dynaloader()
+        {
+        }
+
+        public dynaloader(string dll_name)
+        {
+            load(dll_name);
+        }
+
+        ~dynaloader()
+        {
+            if (loaded()) unload();
+        }
+
+        public bool loaded()
+        {
+            return m_dll != IntPtr.Zero;
+        }
+
+        [DllImport("kernel32.dll")]
+        private static extern IntPtr LoadLibrary(string dllToLoad);
+
+        public bool load(string name)
+        {
+            if (loaded()) unload();
+            m_dll = LoadLibrary(name);
+            MessageBox.Show(loaded().ToString());
+            return loaded();
+        }
+
+        [DllImport("kernel32.dll")]
+        private static extern bool FreeLibrary(IntPtr hModule);
+
+        public bool unload()
+        {
+            if (!loaded()) return true;
+            if (!FreeLibrary(m_dll))
+                return false;
+            m_dll = IntPtr.Zero;
+            return true;
+        }
+
+        [DllImport("kernel32.dll")]
+        private static extern IntPtr GetProcAddress(IntPtr hModule, string procedureName);
+
+        public T load_function<T>(string name) where T : class
+        {
+            IntPtr address = GetProcAddress(m_dll, name);
+            MessageBox.Show(address.ToString());
+            if (address == IntPtr.Zero)
+                return null;
+            System.Delegate fn_ptr = Marshal.GetDelegateForFunctionPointer(address, typeof(T));
+            return fn_ptr as T;
+        }
+    }
 }
